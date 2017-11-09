b3a465e59d9a03fb8011567f38980bb025d648d7
colemai <colemai@tcd.ie>
2017-11-09 12:55:58 +0100

    p3 added mean quality method

--- p3/p3.py
+++ p3/p3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Author: Ian/Cyril
+Author: Ian Coleman
 Purpose:
 """
 
@@ -35,16 +35,16 @@ def record_finder_2(lines):
 			temp_list = []
 	return(master_list)
 
-def mmake_dict(input_list):
-	seqdict=dict()
-	for i in input_list:
-		label = i[0]
-		sequence = i[1]
-		quality = i[3]
-		seqdict[label] = {'Sequence':sequence, 'Quality': quality}
-	print(seqdict['@FCC0U42ACXX:2:1101:17553:4175#ACTACAAG/1'])
+# def mmake_dict(input_list):
+# 	seqdict=dict()
+# 	for i in input_list:
+# 		label = i[0]
+# 		sequence = i[1]
+# 		quality = i[3]
+# 		seqdict[label] = {'Sequence':sequence, 'Quality': quality}
+# 	print(seqdict['@FCC0U42ACXX:2:1101:17553:4175#ACTACAAG/1'])
 
-def mmake_list(input_list):
+def make_list(input_list):
 	seqlist=[]
 	for i in input_list:
 		seq_dict = {}
@@ -59,13 +59,30 @@ def get_quals(listed_fastq):
 		quality_list = []
 		for index in i['quality']:
 			quality_list.append(ord(index) - 64)
-		print (quality_list)
-		exit()
+		i['quality_int'] = quality_list
+	return listed_fastq
 
+def get_lengths(listed_fastq):
+	lengths = []
+	for i in listed_fastq:
+		i['sequence_length'] = len(i['sequence'])
+		lengths.append(len(i['sequence']))
+	length_stats ={'min': min(lengths), 'max':max(lengths), 'mean': sum(lengths)/len(lengths)}
+	return length_stats
 
+def mean_quals(listed_fastq, length_stats):
+	mean_qual_list = []
+	for index in range(0,length_stats['max']):
+		qual_at_position = []
+		for entry in listed_fastq:
+			qual_at_position.append(entry['quality_int'][index])
+		mean_qual_list.append(sum(qual_at_position)/len(qual_at_position))
+	print (mean_qual_list)
 
 if __name__ == "__main__":
 	fastq_lines = intake_fastq(argv[1])
 	fastq_lines_2 = record_finder_2(fastq_lines)
-	listed_fastq = mmake_list(fastq_lines_2)
-	get_quals(listed_fastq)
+	listed_fastq = make_list(fastq_lines_2)
+	listed_fastq_2 = get_quals(listed_fastq)
+	length_stats = get_lengths(listed_fastq_2)
+	mean_quality_list = mean_quals(listed_fastq_2, length_stats)
