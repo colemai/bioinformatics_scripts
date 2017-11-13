60c38bffc40227cc662b70ffbca7dc6407f1e389
colemai <colemai@tcd.ie>
2017-11-13 11:18:08 +0100

    Moving onto parsing needle output

--- p5/p5_ian_coleman.py
+++ p5/p5_ian_coleman.py
@@ -12,6 +12,7 @@ import subprocess
 
 #to do: check Sandra's docstrings
 # a bunch of asserting
+# output identity as float of two decimal places
 
 def parse_fasta (input_filename):
     #Converts fasta file into a dictionary. Input of fasta file.
@@ -22,12 +23,14 @@ def parse_fasta (input_filename):
         #iterate through each line, add label as key and sequence as value
         for i in range(0, len(file_list)):
             if file_list[i].startswith('>'):
+                if fasta_dict: master_seq_list.append(fasta_dict)
+                fasta_dict = {}
                 label_index = file_list[i].strip()
                 fasta_dict['Label'] = label_index
                 fasta_dict['Sequence'] = ''
             else: 
                 fasta_dict['Sequence'] += file_list[i].strip()
-            master_seq_list.append(fasta_dict)
+        master_seq_list.append(fasta_dict)
         return master_seq_list
 
 def add_lengths_to_seqs (seqs_list):
@@ -46,22 +49,55 @@ def call_needle(ref_seq, other_seq, gap_open = 8, gap_extend = 0.5, out_file = '
     err = subprocess.check_output(cmd, shell=True)
     result = subprocess.check_call(cmd, shell=True)
 
-def pairwise_align(ref_seq, seqs):
+def hamming_distance (seq1, seq2):
 	"""
-	Input: ref seq which is a single seq dict in a list, seqs which are multiple
-	Function: Pairwise aligns each of seqs with ref seq using needle programme
-	Output: 
+	Input: Two Sequences of equal length
+	Output: Hamming distance between those two sequences
 	"""
-	for sequence in seqs:
-		reference = ref_seq[1]['Sequence']
-		current_sequence = sequence['Sequence']
+	hamming = 0
+
+	for i in range(0, len(seq1)):
+		if seq1[i] != seq2[i]:
+			hamming +=1
+		else:
+			continue
+	return hamming
+
+def identity_with_ref (ref, seq):
+	"""
+	Input: two sequences
+	Output: identity of the sequences (no identical position/alignment length * 100)
+	Assumes alignment is length of the shorter seq
+	"""
+
+	lengths = [len(ref), len(seq)]
+	alignment_length = min(lengths)
+	identical_positions = hamming_distance(ref, seq)
+	identity = (identical_positions/alignment_length) * 100
+	print (identity)
+
+
+# def pairwise_align(ref_seq, seqs):
+# 	"""
+# 	Input: ref seq which is a single seq dict in a list, seqs which are multiple
+# 	Function: Pairwise aligns each of seqs with ref seq using needle programme
+# 	Output: 
+# 	"""
+# 	for sequence in seqs:
+# 		reference = ref_seq[1]['Sequence']
+# 		current_sequence = sequence['Sequence']
 		
-		call_needle(reference, current_sequence)
+# 		call_needle(reference, current_sequence)
 
 if __name__ == "__main__":
     #run the methods
 	reference_sequence = prep_seqs(argv[1])
 	non_reference_sequences = prep_seqs(argv[2])
+	# print (non_reference_sequences)
 	
-	call_needle(argv[1], argv[2])
+	# call_needle(argv[1], argv[2])
+	# hamming_distance(reference_sequence[0]['Sequence'], non_reference_sequences[2]['Sequence'])
+	identity_with_ref(reference_sequence[0]['Sequence'], non_reference_sequences[1]['Sequence'])
+
+
 
