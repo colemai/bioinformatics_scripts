3d4a56c5b16f49c3c9dfcdeede55c1b4e445fd35
colemai <colemai@tcd.ie>
2018-06-05 12:24:15 +0200

    Mostly finished with Cons, need to format output

--- rosalind/ros17_consensus_and_profile.py
+++ rosalind/ros17_consensus_and_profile.py
@@ -3,6 +3,7 @@
 Author: Ian Coleman
 Input: A Fasta file with multiple DNA seqs of equal length
 Output: Consensus string followed by profile matrix
+Note: Expects uppercase seqs
 
 Challenge description: http://rosalind.info/problems/cons/
 
@@ -10,11 +11,12 @@ Challenge description: http://rosalind.info/problems/cons/
 
 from sys import argv
 import pdb
+import numpy as np
 
 def intake_data(file_path):
 	"""
-	Input: Path to fast file with multiple DNA seqs of equal length
-	Output: Array of seqs
+	Input: Path to fasta file with multiple DNA seqs of equal length
+	Output: Numpy-array of seqs
 	"""
 	with open(file_path, 'r') as file_object:
 		lines_list = file_object.readlines()
@@ -24,17 +26,67 @@ def intake_data(file_path):
 				seq_list.append(line.strip())
 		return seq_list
 
+# def make_profile_matrix(seq_list):
+# 	"""
+# 	Input: Numpy-array of DNA sequences of equal length
+# 	Output: Profile Matrix (4 x length of seqs, as np array)
+# 	"""
+# 	counts = []
+# 	for i in ['A','C','G','T']: counts.append(np.char.count(seq_list, i))
+# 	print(seq_list)
+# 	print(np.vstack(counts))
+
 def make_profile_matrix(seq_list):
 	"""
-	Input: List of DNA sequences of equal length
-	Output: Profile Matrix (4 x length of seqs)
+	Input: Numpy-array of DNA sequences of equal length
+	Output: Profile Matrix (4 x length of seqs, as np array)
 	"""
+
+	labels = ['A','C', 'G','T']
+
+	# Get matrix of individual bases of each seq
+	split_chars = []
+	for seq in seq_list: 
+		split_chars.append(list(seq))
+	base_matrix = np.vstack(split_chars) 
+
+	# Create string of each column as though it were a seq
+	base_matrix_t = np.transpose(base_matrix)
+	columns = []
+	for i in range(0,base_matrix_t.shape[0]):
+		string = ""
+		for base in base_matrix_t[i]:
+			string += base
+		columns.append(string)
+	print(columns)
+
+	# Count and Return profile matrix 
+	counts = []
+	for i in ['A','C','G','T']: counts.append(np.char.count(columns, i))
+	return(np.vstack(counts))
+		
+
+
+
+def make_consensus_seq(profile_matrix):
+	"""
+	Input: Profile Matrix (4 x length of seqs, as np array)
+	Output: Consensus Sequence as string
+	"""
+	labels = {0: 'A', 1: 'C', 2: 'G', 3: 'T'}
+	consensus = ""
+	highest_values = np.argmax(profile_matrix, axis=0)
+	for i in highest_values: 
+		consensus += labels[i]
+	return consensus
+
+
 
 
 
 if __name__ == "__main__":
 	seq_list = intake_data(argv[1])
-	
+
 	#Test that seqs meet criteria
 	assert len(seq_list) > 1
 	for seq in seq_list:
@@ -42,4 +94,6 @@ if __name__ == "__main__":
 		"The sequences are not of equal length..."
 		
 	profile_matrix = make_profile_matrix(seq_list)
+	consensus = make_consensus_seq(profile_matrix)
+	# print(consensus)
 	
