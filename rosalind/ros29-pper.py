e71a108442b5cb4d6622b226965fd8e5d69da6ab
colemai <colemai@tcd.ie>
2018-07-11 21:43:19 +0200

    Ros29-pper completed

--- rosalind/ros29-pper.py
+++ rosalind/ros29-pper.py
@@ -3,7 +3,7 @@
 Author: Ian Coleman
 Purpose: Find partial permutations as per http://rosalind.info/problems/pper/
 Input: Path to txt file, two ints on one line separated by a space
-Output: Int
+Output: Int (number of permutations of k out of n elements)
 """
 
 from sys import argv
@@ -16,9 +16,24 @@ def get_ints_from_file (file_path):
 	"""
 	with open(file_path) as file_object:
 		line = file_object.read()
-	return line.split(' ')
+		n,k = line.split(' ')
+		n = int(n)
+		k = int(k)
+	return n,k
+
+ 
+def factorial (n):
+    return 1 if (n < 1) else n * factorial(n-1)
+
+def combinations (n, r):
+	return factorial(n)/(factorial(r) * factorial(n-r))
 
 
 if __name__ == "__main__":
 	n,k = get_ints_from_file(argv[1])
-	
+	print('n: ', n, 'k: ', k)
+
+	combinations = combinations(n,k)
+	factorial_k = factorial(k)
+
+	print('ANS: ', (factorial_k * combinations) % 1000000)
