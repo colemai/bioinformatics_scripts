ed9b9838ea946278170326019471b2c0691d7367
colemai <colemai@tcd.ie>
2018-07-31 12:18:09 +0200

    Structure in place for ros31-sign

--- rosalind/ros31-sign.py
+++ rosalind/ros31-sign.py
@@ -10,8 +10,28 @@ Briefing: http://rosalind.info/problems/sign/
 from sys import argv
 import pdb
 import numpy as np
+import itertools
 from ian_bif_utilities import get_input_ints
 
+
+
+def permutations(n):
+	"""
+	Input: Int n
+	Output: List of all n lengthed permutations 1..n E R
+	"""
+	original = list(range(1,n+1))
+	return list(itertools.permutations(original))
+
+def signed_perms (perm):
+	"""
+	Input: List of ints (all positive)
+	Output: List of lists of ints (all combinations of pos/neg of input list)
+	"""
+	signed_perms = []
+
+
+	return [1]
 
 
 
@@ -19,3 +39,11 @@ from ian_bif_utilities import get_input_ints
 if __name__ == "__main__":
 	n = get_input_ints(argv[1])[0] #get first and only element from input list
 	print(n)
+
+	perms = permutations(n)
+	print(perms)
+
+	master_list = []
+	for perm in perms:
+		master_list += signed_perms(perm)
+	print(master_list)
