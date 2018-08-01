23bb8e45da7bb977b1a85cd3574205a3abcf0a62
colemai <colemai@tcd.ie>
2018-08-01 11:14:56 +0200

    Finished ros32

--- rosalind/ros32-sseq.py
+++ rosalind/ros32-sseq.py
@@ -12,7 +12,27 @@ from sys import argv
 
 from ian_bif_utilities import fasta_to_list
 
+def find_subsequence (seq, subseq):
+	"""
+	Input: Two Strings, one is a subseq of the other (maybe not contiguous)
+	Output: List (indices of subseq in seq)
+	"""
+	indices = [] # create output list
+
+	current_index = -1 # use this as iterator to ensure correct order
+	cut_length = 0 # this to account for the bits I cut off in iterating
+	# iterate through the letters in subseq, find first occurence in seq
+	for base in subseq:
+		current_index = seq[current_index + 1:].find(base) + cut_length 
+		indices.append(current_index)
+		cut_length = current_index + 1
+	
+	indices = [i + 1 for i in indices] # they don't want zero-indexed
+	return(indices)
+
 
 if __name__ == "__main__":
 	seq_t, seq_s = fasta_to_list(argv[1])
-	print(seq_t, seq_s)
+
+	indices = find_subsequence(seq_t, seq_s)
+	print(*indices)
