776cdeeacc4a82c89de14fbef23aca12934046e8
colemai <colemai@tcd.ie>
2019-01-02 16:57:20 +0000

    Have correct seqs, incorrect seqs

--- rosalind/ros36-corr.py
+++ rosalind/ros36-corr.py
@@ -24,20 +24,29 @@ if __name__ == "__main__":
 	# Get all correct sequences
 	# Create a set of correct seqs then add all their revcomps
 
-	correct_sequences = []
+	correct_sequences = set()
 	temp_seqs = seqs[:] # as I'll be removing elements
 	for sequence in seqs:
 		temp_seqs.remove(sequence)
 		# pdb.set_trace()
 		rev_comp = generate_reverse_complement_dna(sequence)
 		if sequence in temp_seqs:
-			correct_sequences.append(sequence)
+			correct_sequences.add(sequence)
 		elif rev_comp in temp_seqs:
-			correct_sequences.append(sequence)
+			correct_sequences.add(sequence)
 
 	print(correct_sequences)
-	# By inference, all incorrect sequences
+	
 	# Add reverse complement for each correct sequence to correct list
+	correct_seq_revcomps = {generate_reverse_complement_dna(item) for item in correct_sequences}
+	correct_sequences = correct_sequences.union(correct_seq_revcomps)
+	print('Correct Sequences: ', correct_sequences)
 
+	# By inference, all incorrect sequences
+	incorrect_seqs = [item for item in seqs if item not in correct_sequences]
+	print('Incorrect Seqs: ', incorrect_seqs)
+
+	# Now we have all the incorrect seqs as well as the correct seqs + revcomps
+	
 	# Then hard bit, iterate through incorrects and match them with a correct
 	# of hamming distance 1 (will only be one apparently)
